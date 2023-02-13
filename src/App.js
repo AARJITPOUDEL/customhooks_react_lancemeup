21fbfc95d07af8b427c31b6ff46c8fce3528fe20
Aarjit <poudelaarjit@gmail.com>
2023-02-13 20:53:42 +0545

    final changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
-const useLocalStorage = (key, initialValue) => {
+import  './App.css';
+const useAarjitCustomHook = (key, initialValue) => {
   const [value, setValue] = useState(() => {
     try {
       const item = window.localStorage.getItem(key);
@@ -8,7 +8,7 @@ const useLocalStorage = (key, initialValue) => {
     } catch (error) {
       console.error(error);
       return initialValue;
-    }
+    } 
   });
 
   useEffect(() => {
@@ -16,16 +16,11 @@ const useLocalStorage = (key, initialValue) => {
       window.localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
       console.error(error);
-    }
+    } 
   }, [key, value]);
 
-  const removeItem = () => {
-    setValue(initialValue);
-    try {
-      window.localStorage.removeItem(key);
-    } catch (error) {
-      console.error(error);
-    }
+  const removeItem = (index) => {
+    setValue(value.filter((_, i) => i !== index));
   };
 
   const clear = () => {
@@ -37,27 +32,51 @@ const useLocalStorage = (key, initialValue) => {
     }
   };
 
-  const getValue = () => {
-    try {
-      return window.localStorage.getItem(key);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return [value, setValue, removeItem, clear, getValue];
+  return [value, setValue, removeItem, clear];
 };
 
 const App = () => {
-  const [name, setName, removeName, clearName, getName] = useLocalStorage("name", "");
+  const [applicants, setApplicants, removeApplicant, clearApplicants] = useAarjitCustomHook("applicants", []);
+
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setApplicants([...applicants, { name, phone }]);
+    setName("");
+    setPhone("");
+  };
 
   return (
     <div>
-      <p>Name: {name}</p>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      <button onClick={removeName}>Remove Name</button>
-      <button onClick={clearName}>Clear all</button>
-      <p>Value from local storage: {getName()}</p>
+      <h2>Using Custom Hook for data handling</h2>
+      {/* This is a little demo for the problem provided by LancemeUp */}
+      <ul>
+        {applicants.map((applicant, index) => (
+          <li key={index}>
+            <p>Name: {applicant.name}</p>
+            <p>Phone: {applicant.phone}</p>
+            <button onClick={() => removeApplicant(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <button type="submit">Add Applicant For Internship</button>
+      </form>
+      <button onClick={clearApplicants} className="button" > Clear all</button>
     </div>
   );
 };
